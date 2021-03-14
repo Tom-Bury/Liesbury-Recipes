db77daba1fb2954e8046eaab8f3da9f592819dfe
Tom Bury <tom.bury@twipemobile.com>
2021-03-14 22:41:50 +0100

    Add layouts to tailwind purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    tailwindcss: {},
-    'postcss-nested': {},
-    autoprefixer: {}
-  }
+  plugins: ['postcss-import', 'tailwindcss', 'postcss-nested', 'autoprefixer']
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 // https://coolors.co/005259-005a62-00636c-006d77-83c5be-d5eceb-edf6f9-e8c6b9-e29578
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './layouts/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
