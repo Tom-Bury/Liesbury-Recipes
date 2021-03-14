5a244b192ad368b2564d7c365842d0b114c0185d
Tom Bury <tom.bury@twipemobile.com>
2021-03-14 21:54:39 +0100

    Temp fix typing issue

--- components/LinkWrap.tsx
+++ components/LinkWrap.tsx
@@ -7,7 +7,7 @@ type TProps = React.PropsWithChildren<{
   className?: string
 }>
 
-const LinkWrap: React.ForwardRefExoticComponent<TProps> = React.forwardRef(({ id, href, className, children }, ref) => {
+const LinkWrap: React.ForwardRefExoticComponent<TProps> = React.forwardRef(({ id, href, className, children }, ref: any) => {
   return (
     <Link key={id} href={href} passHref>
       <a className={className} href={href} ref={ref}>
