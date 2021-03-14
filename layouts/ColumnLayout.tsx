b19fb5ffbb8a862813d74f20accfd1133bf5e64b
Tom Bury <tom.bury@twipemobile.com>
2021-03-14 22:46:29 +0100

    Add bottom padding on index

--- layouts/ColumnLayout.tsx
+++ layouts/ColumnLayout.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react'
+import classNames from 'classnames'
 
 interface TProps {
-  classNames?: string
+  className?: string
 }
 
-const defaultProps: TProps = {
-  classNames: ''
+const ColumnLayout: React.FC<TProps> = ({ children, className }) => {
+  const classes = classNames('mx-auto px-4', className)
+  return <div className={classes}>{children}</div>
 }
-
-const ColumnLayout: React.FC<TProps> = ({ children, classNames }) => {
-  const baseClasses = 'mx-auto px-4'
-  return <div className={`${baseClasses} ${classNames}`}>{children}</div>
-}
-
-ColumnLayout.defaultProps = defaultProps
 
 export default ColumnLayout
