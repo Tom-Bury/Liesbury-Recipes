89619fd3b3e1e998deba684b452e593bcd45b400
Tom Bury <tom.bury@twipemobile.com>
2021-03-15 20:11:09 +0100

    Add log fetching endpoint

--- types/enums.ts
+++ types/enums.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+
 export enum ECategory {
   MEAT = 'MEAT',
   FISH = 'FISH',
@@ -7,9 +9,11 @@ export enum ECategory {
 }
 
 export enum EErrorCode {
-  E1 = 'E1'
+  E1 = 'E1',
+  E2 = 'E2'
 }
 
 export enum EErrorMessage {
-  E1 = 'Error while fetching data'
+  E1 = 'Error while fetching data',
+  E2 = 'Error while fetching logfile'
 }
