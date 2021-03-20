2b62408d6afd0a2261fa9d038abda955fb105f30
Tom Bury <tom.bury@twipemobile.com>
2021-03-20 21:09:29 +0100

    Use google cloud storage bucket for preview images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   target: process.env.BUILD_TARGET || 'server',
   images: {
-    domains: ['images.immediate.co.uk', 'img.hellofresh.com'],
-  },
+    domains: ['storage.googleapis.com']
+  }
 }
---
+++ services/StorageService.ts
@@ -0,0 +1,38 @@
+import { Storage, Bucket } from '@google-cloud/storage'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+class StorageService {
+  private bucket: Bucket
+
+  public bucketUrl = `https://storage.googleapis.com/${process.env.GCS_BUCKET_NAME}`
+
+  constructor() {
+    const bucketName = process.env.GCS_BUCKET_NAME
+    const secret = JSON.parse(Buffer.from(process.env.GCS_SERVICE_ACCOUNT as string, 'base64').toString())
+    const storage = new Storage({
+      projectId: process.env.GC_PROJECT_ID,
+      credentials: {
+        client_email: secret.client_email,
+        client_id: secret.client_id,
+        private_key: secret.private_key
+      }
+    })
+    this.bucket = storage.bucket(bucketName as string)
+  }
+
+  public async uploadFile(file: Buffer, fileName: string) {
+    const newFileRef = this.bucket.file(fileName)
+    await newFileRef.save(file)
+  }
+
+  public async fileExists(fileName: string) {
+    const fileRef = this.bucket.file(fileName)
+    const response = await fileRef.exists()
+    const exists = response[0]
+    return exists
+  }
+}
+
+export default new StorageService()
