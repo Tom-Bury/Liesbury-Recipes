d4ed40708ebe0f57bc2730e02ec667d60a3bc63a
Tom Bury <tom.bury@twipemobile.com>
2021-03-21 11:20:55 +0100

    Remove fetch retry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,6 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/globals.css'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fetch2 = require('@vercel/fetch-retry')(require('node-fetch'))
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch2
-}
-
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
