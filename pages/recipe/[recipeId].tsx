0766fa9655fa332fcf7e2b96f36707103fc4b73c
Tom Bury <tom.bury@twipemobile.com>
2021-03-28 21:10:56 +0200

    Beginnings of recipe page

--- pages/recipe/[recipeId].tsx
+++ pages/recipe/[recipeId].tsx
@@ -2,11 +2,13 @@ import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import * as React from 'react'
 import { TRecipe } from 'types/recipe.type'
+import Image from 'next/image'
 import Card from '~/components/Card/Card'
 import ElementTitle from '~/components/ElementTitle'
 import LinkWrap from '~/components/LinkWrap'
 import SectionTitle from '~/components/SectionTitle'
 import { getAllRecipes } from '~/utils/recipesData.utils'
+import LinkCard from '~/components/LinkCard/LinkCard'
 
 type TProps = {
   recipe: TRecipe | undefined
@@ -32,32 +34,27 @@ const RecipePage: NextPage<TProps> = ({ recipe }) => {
   const indices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   return (
-    <div className="flex flex-1 flex-col items-center">
-      <div className="max-w-6xl w-full flex flex-1 justify-center">
-        <div className="max-w-5xl w-full h-80 fixed top-0 z-0" style={style} />
-        <div className="test flex flex-1 flex-col z-10 mt-72 p-8 pt-8 bg-lightest items-center">
-          <div className="sticky w-full top-0 pt-8 bg-lightest flex flex-col flex-1 items-center z-10">
+    <div className="flex flex-1 justify-center">
+      <div className="max-w-5xl w-full h-80 fixed top-0 z-0" style={style} />
+      <div className="rooftop flex flex-1 z-10 mt-72 pt-8 bg-lightest items-center">
+        <div className="flex flex-col flex-1 max-w-5xl p-8 lg:p-0 pt-0 mx-auto">
+          <div className="sticky top-0 pt-8 bg-lightest flex flex-col flex-1 items-center z-10">
             <SectionTitle>{recipe.title}</SectionTitle>
-            <hr className="border-t-4 border-primary max-w-5xl w-full" />
+            <hr className="border-t-4 border-primary w-full" />
           </div>
-          <div className="max-w-5xl w-full flex flex-1 flex-col items-start pt-8">
+          <div className="flex flex-1 flex-col items-start pt-8">
             <ElementTitle>Original recipe</ElementTitle>
-            <LinkWrap href={recipe.url}>
-              <Card hoverable className="p-8 m-8">
-                <h4>To original website</h4>
-              </Card>
-            </LinkWrap>
-
+            <LinkCard url={recipe.url} title="To original recipe website" />
             <ElementTitle>Ingredients</ElementTitle>
             {indices.map(i => (
               <div key={i} className="h-20 w-20 bg-dark p-8 m-8">
-                {i}
+                {`todo ${i}`}
               </div>
             ))}
             <ElementTitle>Recipe</ElementTitle>
             {indices.map(i => (
               <div key={i} className="h-20 w-20 bg-dark p-8 m-8">
-                {i}
+                {`todo ${i}`}
               </div>
             ))}
           </div>
