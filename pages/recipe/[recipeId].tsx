c526b0882837835545d6bb3c2a66fba53885cb7c
Tom Bury <tom.bury@twipemobile.com>
2021-05-01 16:45:33 +0200

    Split recipe page + add ISR

--- pages/recipe/[recipeId].tsx
+++ pages/recipe/[recipeId].tsx
@@ -1,14 +1,13 @@
+import { HorizontalCenterLayout } from 'layouts'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import * as React from 'react'
-import { TRecipe } from 'types/recipe.type'
 import remark from 'remark'
 import html from 'remark-html'
-import { HorizontalCenterLayout } from 'layouts'
-import LinkCard from '~/components/LinkCard/LinkCard'
+import { TRecipe } from 'types/recipe.type'
+import RecipeData from '~/components/RecipeData'
+import RecipePlaceholder from '~/components/RecipePlaceholder'
 import { getAllRecipes, getRecipeInstructionsMarkdown } from '~/utils/recipesData.utils'
-import MarkdownSnippet from '~/components/MarkdownSnippet/MarkdownSnippet'
-import Loading from '~/components/Loading'
 
 type TProps = {
   recipe: TRecipe | undefined
@@ -16,15 +15,8 @@ type TProps = {
 }
 
 const RecipePage: NextPage<TProps> = ({ recipe, instructions }) => {
-  const router = useRouter()
-  if (router.isFallback) {
-    return (
-      <HorizontalCenterLayout className="h-screen justify-center">
-        <Loading />
-        <h3 className="text-dark">Laden...</h3>
-      </HorizontalCenterLayout>
-    )
-  }
+  const { isFallback } = useRouter()
+
   if (!recipe) {
     return (
       <HorizontalCenterLayout className="h-screen justify-center">
@@ -33,7 +25,7 @@ const RecipePage: NextPage<TProps> = ({ recipe, instructions }) => {
     )
   }
 
-  const style = {
+  const recipeImgStyle = {
     background: `url(${recipe.imgPath})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover'
@@ -48,40 +40,14 @@ const RecipePage: NextPage<TProps> = ({ recipe, instructions }) => {
 
   return (
     <div className="flex flex-1 justify-center">
-      <div className="max-w-5xl w-full h-80 fixed top-0 z-0" style={style} />
+      <div className="max-w-5xl w-full h-80 fixed top-0 z-0" style={recipeImgStyle} />
       <div className="rooftop flex flex-1 z-10 mt-72 lg:mb-16 pt-8 bg-lightest items-center">
         <div className="flex flex-col flex-1 max-w-5xl pt-0 mx-auto">
           <div className="sticky top-0 pt-8 bg-lightest flex flex-col flex-1 items-center z-10">
-            <h2 className="text-darkest text-center px-2">{recipe.title}</h2>
+            <h2 className="text-darkest text-center px-2">{isFallback ? '...' : recipe.title}</h2>
             <hr className="border-t-4 border-primary w-full" />
           </div>
-          <div className="grid grid-cols-1 gap-y-8 pt-8">
-            {recipeUrl?.hostname && (
-              <section className="w-full flex flex-col items-start px-8">
-                <h3 className="text-dark">Origineel recept </h3>
-                <LinkCard url={recipeUrl} title={new URL(recipe.url).hostname} className="mx-8 my-4" />
-              </section>
-            )}
-
-            {recipe.ingredients && (
-              <section className="w-full flex flex-col items-start px-8">
-                <h3 className="text-dark">Ingrediënten</h3>
-                <ul className="list-disc list-inside px-4 my-4 text-dark">
-                  {recipe.ingredients.split(',').map(i => (
-                    <li key={i} className="text-lg">
-                      {i.trim()}
-                    </li>
-                  ))}
-                </ul>
-              </section>
-            )}
-
-            {instructions && (
-              <section className="w-full flex flex-col items-start bg-light">
-                <MarkdownSnippet instructionsHtml={instructions} className="p-8 py-16 text-darkest" />
-              </section>
-            )}
-          </div>
+          {isFallback ? <RecipePlaceholder /> : <RecipeData recipe={recipe} recipeUrl={recipeUrl} instructions={instructions} />}
         </div>
       </div>
     </div>
@@ -102,7 +68,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       recipe,
       instructions
-    }
+    },
+    revalidate: 60 * 5 // Regenerate the page when a new request comes in, at most each 5 minutes
   }
 }
 
--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,136 @@
-if(!self.define){const e=e=>{"require"!==e&&(e+=".js");let s=Promise.resolve();return r[e]||(s=new Promise((async s=>{if("document"in self){const r=document.createElement("script");r.src=e,document.head.appendChild(r),r.onload=s}else importScripts(e),s()}))),s.then((()=>{if(!r[e])throw new Error(`Module ${e} didn’t register its module`);return r[e]}))},s=(s,r)=>{Promise.all(s.map(e)).then((e=>r(1===e.length?e[0]:e)))},r={require:Promise.resolve(s)};self.define=(s,a,i)=>{r[s]||(r[s]=Promise.resolve().then((()=>{let r={};const n={uri:location.origin+s.slice(1)};return Promise.all(a.map((s=>{switch(s){case"exports":return r;case"module":return n;default:return e(s)}}))).then((e=>{const s=i(...e);return r.default||(r.default=s),r}))})))}}define("./sw.js",["./workbox-ea903bce"],(function(e){"use strict";importScripts("fallback-E-4RUCMal14XCiNbehBwA.js"),self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"/_next/static/E-4RUCMal14XCiNbehBwA/_buildManifest.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/E-4RUCMal14XCiNbehBwA/_ssgManifest.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/282-26b8230105c1a7e1e51b.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/288-64b780f86435c5d27307.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/942-a352c8a14f2e6a25a037.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/framework-737b57d65c7ddec02a17.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/main-0e72df510668a92c270f.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/pages/_app-eb59f297f59a023d47ed.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/pages/_error-de599369f9207bac2e35.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/pages/_offline-ef120ee13bb30f9353a2.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/pages/index-69972d551b8958ad8586.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/pages/recipe/%5BrecipeId%5D-27bfd34465c3aaeed7e9.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/polyfills-8683bd742a84c1edd48c.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/chunks/webpack-4755d1c0768947b7172e.js",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/css/0b4aaa461920a10d362b.css",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/css/5118ffc6ff9cb06de5df.css",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_next/static/css/9272d16463cf6ad67307.css",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/_offline",revision:"E-4RUCMal14XCiNbehBwA"},{url:"/favicon.ico",revision:"54dba7508af2e0a4392cb6510888310f"},{url:"/icons/liesbury-192.png",revision:"6a52ba0430ae23387f5a2db3420ba2db"},{url:"/icons/liesbury-512.png",revision:"b3d1b2301da9aee1faa615d5266082d8"},{url:"/icons/liesbury_maskable_icon_x128.png",revision:"25bab6541268c77c67fbfd0302cc4ae9"},{url:"/icons/liesbury_maskable_icon_x192.png",revision:"c4219acc1b02ea0c79e778e913ae822c"},{url:"/icons/liesbury_maskable_icon_x384.png",revision:"6d9abb058d4b7a31c88be27c2d067a6d"},{url:"/icons/liesbury_maskable_icon_x48.png",revision:"78dc46db047163e5298bfda4257354e5"},{url:"/icons/liesbury_maskable_icon_x512.png",revision:"e738234875b6b6c645863effc4fd7de7"},{url:"/icons/liesbury_maskable_icon_x72.png",revision:"4e3a66d01d0632716366687eb0d893e5"},{url:"/icons/liesbury_maskable_icon_x96.png",revision:"0e7efd0942f1decbbc1f284a6c325150"},{url:"/icons/link.svg",revision:"051783ac3935bf70397fcd0a6b9d0456"},{url:"/icons/search.svg",revision:"84433ae7afa0c709a11913af7fbf87ea"},{url:"/images/liesbury-og-image.png",revision:"534156f9d7bd5c3a176db09c9446eed1"},{url:"/images/liesbury-recipes-colored.svg",revision:"5664f7c9e73c5457e5ebb8e1ba53d65b"},{url:"/manifest.json",revision:"a96a50cae9b2586e48926f6c5a3f9514"}],{ignoreURLParametersMatching:[]}),e.cleanupOutdatedCaches(),e.registerRoute("/",new e.NetworkFirst({cacheName:"start-url",plugins:[{cacheWillUpdate:async({request:e,response:s,event:r,state:a})=>s&&"opaqueredirect"===s.type?new Response(s.body,{status:200,statusText:"OK",headers:s.headers}):s},{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,new e.CacheFirst({cacheName:"google-fonts",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:31536e3,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,new e.StaleWhileRevalidate({cacheName:"static-font-assets",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:604800,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,new e.StaleWhileRevalidate({cacheName:"static-image-assets",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:64,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\/_next\/image\?url=.+$/i,new e.StaleWhileRevalidate({cacheName:"next-image",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:64,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\.(?:mp3|mp4)$/i,new e.StaleWhileRevalidate({cacheName:"static-media-assets",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\.(?:js)$/i,new e.StaleWhileRevalidate({cacheName:"static-js-assets",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\.(?:css|less)$/i,new e.StaleWhileRevalidate({cacheName:"static-style-assets",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\/_next\/data\/.+\/.+\.json$/i,new e.StaleWhileRevalidate({cacheName:"next-data",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute(/\.(?:json|xml|csv)$/i,new e.NetworkFirst({cacheName:"static-data-assets",plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute((({url:e})=>{if(!(self.origin===e.origin))return!1;const s=e.pathname;return!s.startsWith("/api/auth/")&&!!s.startsWith("/api/")}),new e.NetworkFirst({cacheName:"apis",networkTimeoutSeconds:10,plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:16,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET"),e.registerRoute((({url:e})=>{if(!(self.origin===e.origin))return!1;return!e.pathname.startsWith("/api/")}),new e.NetworkFirst({cacheName:"others",networkTimeoutSeconds:10,plugins:[{handlerDidError:async({request:e})=>self.fallback(e)},new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400,purgeOnQuotaError:!0})]}),"GET")}));
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  const singleRequire = name => {
+    if (name !== 'require') {
+      name = name + '.js';
+    }
+    let promise = Promise.resolve();
+    if (!registry[name]) {
+      
+        promise = new Promise(async resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = name;
+            document.head.appendChild(script);
+            script.onload = resolve;
+          } else {
+            importScripts(name);
+            resolve();
+          }
+        });
+      
+    }
+    return promise.then(() => {
+      if (!registry[name]) {
+        throw new Error(`Module ${name} didn’t register its module`);
+      }
+      return registry[name];
+    });
+  };
+
+  const require = (names, resolve) => {
+    Promise.all(names.map(singleRequire))
+      .then(modules => resolve(modules.length === 1 ? modules[0] : modules));
+  };
+  
+  const registry = {
+    require: Promise.resolve(require)
+  };
+
+  self.define = (moduleName, depsNames, factory) => {
+    if (registry[moduleName]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    registry[moduleName] = Promise.resolve().then(() => {
+      let exports = {};
+      const module = {
+        uri: location.origin + moduleName.slice(1)
+      };
+      return Promise.all(
+        depsNames.map(depName => {
+          switch(depName) {
+            case "exports":
+              return exports;
+            case "module":
+              return module;
+            default:
+              return singleRequire(depName);
+          }
+        })
+      ).then(deps => {
+        const facValue = factory(...deps);
+        if(!exports.default) {
+          exports.default = facValue;
+        }
+        return exports;
+      });
+    });
+  };
+}
+define("./sw.js",['./workbox-6b19f60b'], function (workbox) { 'use strict';
+
+  /**
+  * Welcome to your Workbox-powered service worker!
+  *
+  * You'll need to register this file in your web app.
+  * See https://goo.gl/nhQhGp
+  *
+  * The rest of the code is auto-generated. Please don't update this file
+  * directly; instead, make changes to your Workbox build configuration
+  * and re-run your build process.
+  * See https://goo.gl/2aRDsh
+  */
+
+  importScripts("fallback-development.js");
+  self.skipWaiting();
+  workbox.clientsClaim();
+  workbox.registerRoute("/", new workbox.NetworkFirst({
+    "cacheName": "start-url",
+    plugins: [{
+      cacheWillUpdate: async ({
+        request,
+        response,
+        event,
+        state
+      }) => {
+        if (response && response.type === 'opaqueredirect') {
+          return new Response(response.body, {
+            status: 200,
+            statusText: 'OK',
+            headers: response.headers
+          });
+        }
+
+        return response;
+      }
+    }, {
+      handlerDidError: async ({
+        request
+      }) => self.fallback(request)
+    }]
+  }), 'GET');
+  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
+    "cacheName": "dev",
+    plugins: [{
+      handlerDidError: async ({
+        request
+      }) => self.fallback(request)
+    }]
+  }), 'GET');
+
+});
+//# sourceMappingURL=sw.js.map
