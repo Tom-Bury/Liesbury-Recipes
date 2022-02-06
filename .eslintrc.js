b1ef91f5dcb111c4cf7d31a012421b8fcae7fecf
Tom Bury <tom.bury@twipemobile.com>
2022-02-06 17:29:59 +0100

    feat: adjust linter rules + add icons & global styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,8 @@ module.exports = {
         '@typescript-eslint/camelcase': 'off',
         '@typescript-eslint/explicit-function-return-type': ['off'],
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        'import/prefer-default-export': 'off'
       }
     },
     {
