41d6493cfaa1b87e110be8064aabb62b70a077d6
Tom Bury <tom.bury@twipemobile.com>
2022-08-08 23:04:41 +0200

    feat: add blurhash to recipe card

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -4,16 +4,27 @@ import Card from './Card/Card'
 
 type TProps = {
   title: string
-  imgPath?: string
   preloadImage: boolean
+  imgPath?: string
+  blurHash?: string
 }
 
-const RecipeCard: React.FC<TProps> = ({ title, imgPath, preloadImage }) => {
+const RecipeCard: React.FC<TProps> = ({ title, imgPath, preloadImage, blurHash }) => {
   return (
     <Card className="rmMobileClickBox" hoverable gradient>
       <div className="flex flex-col h-80 p-4">
         <div className="w-full h-60 relative">
-          {imgPath && <Image className="rounded-xl" src={imgPath} alt={title} layout="fill" objectFit="cover" priority={preloadImage} />}
+          {imgPath && (
+            <Image
+              className="rounded-xl"
+              src={imgPath}
+              alt={title}
+              layout="fill"
+              objectFit="cover"
+              priority={preloadImage}
+              blurDataURL={blurHash}
+            />
+          )}
           {!imgPath && <div className="rounded-xl bg-light shadow-inner w-full h-full" />}
         </div>
         <div className="flex-1 flex-col p-6">
