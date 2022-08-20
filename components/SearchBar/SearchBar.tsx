3767218850bd4406adef2d9f79116d790172952c
Tom Bury <tom.bury@twipemobile.com>
2022-08-20 19:09:44 +0200

    feat: warm up search function on focus

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Image from 'next/image'
 import classNames from 'classnames'
+import { useState } from 'react'
+import searchRecipes from 'api/searchRecipes'
 import styles from './SearchBar.module.css'
 
 type TProps = {
@@ -9,12 +11,22 @@ type TProps = {
 }
 
 const SearchBar: React.FC<TProps> = ({ placeholder, onSearch }) => {
+  const [hasWarmedSearchApi, setHasWarmedSearchApi] = useState(false)
+
   const search = async (event: any) => {
     event.preventDefault()
     document.getElementById('search')?.blur()
     const query = event.target.search.value
     if (query) {
       onSearch(query)
+    }
+  }
+
+  const onFocus = () => {
+    // Combatting cold start of the search function in a hacky way
+    if (!hasWarmedSearchApi) {
+      searchRecipes('')
+      setHasWarmedSearchApi(true)
     }
   }
 
@@ -30,6 +42,7 @@ const SearchBar: React.FC<TProps> = ({ placeholder, onSearch }) => {
           'bg-primary h-12 w-full text-white px-5 pr-10 rounded-full text-md focus:outline-none focus:ring-4 focus:ring-dark focus:ring-opacity-50 transition duration-150',
           styles.input
         )}
+        onFocus={onFocus}
       />
       <button type="submit" className="absolute right-0 top-0 flex h-full mr-4">
         <div className="flex h-full justify-content-center align-items-center">
