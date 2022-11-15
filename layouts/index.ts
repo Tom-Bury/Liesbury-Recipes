743847b621461c169ea3d02924fdcd1b19e19aed
Tom Bury <tom.bury@twipemobile.com>
2022-11-15 22:57:14 +0100

    chore: remove ColumnLayout

--- layouts/index.ts
+++ layouts/index.ts
@@ -1,3 +1,2 @@
-export { default as ColumnLayout } from './ColumnLayout'
 export { default as HorizontalCenterLayout } from './HorizontalCenterLayout'
 export { default as GridLayout } from './GridLayout'
