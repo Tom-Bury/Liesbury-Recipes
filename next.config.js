129eda4c0ce73d209da93cd932f567214adc813a
Tom Bury <tom.bury@twipemobile.com>
2024-03-03 13:00:48 +0100

    temp: disable pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
-})
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development'
+// })
 
-module.exports = withPWA({
+module.exports = {
   productionBrowserSourceMaps: true,
   images: {
     domains: ['storage.googleapis.com']
   }
-})
+}
