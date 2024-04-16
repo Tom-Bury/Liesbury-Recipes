a63aa7ebe9052c834dd92fa2458a30c41f9f65ba
Tom Bury <tom.bury@hotmail.com>
2024-04-16 22:49:42 +0200

    feat: speed up animation of recipe list with each new card

--- components/RecipeList.tsx
+++ components/RecipeList.tsx
@@ -58,7 +58,7 @@ const useAnimatedRecipes = (
     return { animationClass: '', currRecipes, animationDelay: 0 }
   }
 
-  return { animationClass: fadeOut ? 'animate-fade-out' : 'animate-fade-in-left', currRecipes, animationDelay: fadeOut ? 0 : 0.1 }
+  return { animationClass: fadeOut ? 'animate-fade-out' : 'animate-fade-in-left', currRecipes, animationDelay: fadeOut ? 0 : 0.13 }
 }
 
 const RecipeList: React.FC<TProps> = ({ recipes, scrollToRecipeWithId, className, onRecipeClick }) => {
@@ -89,7 +89,7 @@ const RecipeList: React.FC<TProps> = ({ recipes, scrollToRecipeWithId, className
             className="rmMobileClickBox w-full"
             onClick={onRecipeClick ? () => onRecipeClick(recipe.id) : undefined}
           >
-            <div className={animationClass} style={{ animationDelay: `${i * animationDelay}s` }}>
+            <div className={animationClass} style={{ animationDelay: `${Math.sqrt(i + 1) * animationDelay}s` }}>
               <RecipeCard title={recipe.title} imgPath={recipe.imgUrl} blurHash={recipe.blurHash} preloadImage={i <= 9} />
             </div>
           </LinkWrap>
@@ -97,7 +97,7 @@ const RecipeList: React.FC<TProps> = ({ recipes, scrollToRecipeWithId, className
       {currRecipes === undefined &&
         Array.from(Array(10)).map((_, i) => {
           return (
-            <div className={animationClass} style={{ animationDelay: `${i * animationDelay}s` }}>
+            <div className={animationClass} style={{ animationDelay: `${Math.sqrt(i + 1) * animationDelay}s` }}>
               <PlaceholderRecipeCard key={`placeholder_${i}`} />
             </div>
           )
